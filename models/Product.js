d74d1e51e0000919a88a4083f948d4f2e14f74a8
Vaibhav Gupta <guptavaibhav432@gmail.com>
2022-12-15 14:39:09 +0530

    Temperory images required false

--- models/Product.js
+++ models/Product.js
@@ -15,7 +15,7 @@ const productSchema = new mongoose.Schema({
     },
     image: {
         type: [String],
-        required: true,
+        // required: true,
     },
     category: {
         type: String,
