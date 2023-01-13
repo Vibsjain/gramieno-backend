bb0e209c2f3e932ab4866861c7728b1927c3fc09
Vaibhav Gupta <guptavaibhav432@gmail.com>
2023-01-14 00:37:43 +0530

    Change Schema for Products for Images

--- models/Product.js
+++ models/Product.js
@@ -13,20 +13,8 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    image1: {
-        type: String,
-        required: true,
-    },
-    image2: {
-        type: String,
-        required: true,
-    },
-    image3: {
-        type: String,
-        required: true,
-    },
-    image4: {
-        type: String,
+    images: {
+        type: [String],
         required: true,
     },
     category: {
