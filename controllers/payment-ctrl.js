65986ba37d1288d59da39a01e2f5955ef01ef7cf
Vaibhav Gupta <guptavaibhav432@gmail.com>
2023-01-21 17:11:59 +0530

    Ctrls checked and payment updated

--- controllers/payment-ctrl.js
+++ controllers/payment-ctrl.js
@@ -34,16 +34,18 @@ exports.paymentVerification = async (req, res) => {
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
         req.body;
     const body = razorpay_order_id + "|" + razorpay_payment_id;
-    const generated_signature = crypto.create.h
-        .mac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    const generated_signature = crypto
+        .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
         .update(body.toString())
         .digest("hex");
     if (generated_signature === razorpay_signature) {
         res.json({
+            success: true,
             message: "Payment Success",
         });
     }
     res.json({
+        success: false,
         message: "Payment Failed",
     });
 };
--- controllers/product-ctrl.js
+++ controllers/product-ctrl.js
@@ -4,9 +4,10 @@ const Product = require("../models/Product");
 exports.getProducts = async (req, res, next) => {
     try {
         const products = await Product.find();
+        console.log(products);
         res.status(200).json(products);
     } catch (err) {
-        res.status(500).json({ message: "Something went wrong" });
+        res.status(500).json({ message: err.message });
     }
 };
 
