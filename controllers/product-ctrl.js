518e7306b2fb6f33c239c7f509f3891c74671857
Vaibhav Gupta <vaibhav.gupta1@prodo.in>
2023-01-29 17:33:40 +0530

    Remove console.log():

--- controllers/product-ctrl.js
+++ controllers/product-ctrl.js
@@ -4,7 +4,6 @@ const Product = require("../models/Product");
 exports.getProducts = async (req, res, next) => {
     try {
         const products = await Product.find();
-        console.log(products);
         res.status(200).json(products);
     } catch (err) {
         res.status(500).json({ message: err.message });
