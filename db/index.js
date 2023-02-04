19d2a13910ebfa58e4215e225244596b7182c1cf
Vaibhav Gupta <vaibhav.gupta1@prodo.in>
2023-02-04 10:00:43 +0530

    MONGO URI CHANGED

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 const chalk = require("chalk");
 require("dotenv").config();
-const URI = process.env.MONGO_URI_DEV;
-// const URI = process.env.MONGO_URI_PROD;
+// const URI = process.env.MONGO_URI_DEV;
+const URI = process.env.MONGO_URI_PROD;
 
 const connnect = (async) => {
     mongoose
